ad4eb0db0ad6a79f923aff50b10a436e5ee70198
Lightbringer <lighter0610@gmail.com>
2022-04-07 21:05:24 -0400

    Add readme

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import torch
+import numpy as np
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+from LIDC.load_LIDC import LIDC_IDRI
+from prob import ProbabilisticUnet
+from utils import l2_regularisation
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+dataset = LIDC_IDRI(dataset_location = 'data/')
+dataset_size = len(dataset)
+indices = list(range(dataset_size))
+split = int(np.floor(0.1 * dataset_size))
+np.random.shuffle(indices)
+train_indices, test_indices = indices[split:], indices[:split]
+train_sampler = SubsetRandomSampler(train_indices)
+test_sampler = SubsetRandomSampler(test_indices)
+train_loader = DataLoader(dataset, batch_size=5, sampler=train_sampler)
+test_loader = DataLoader(dataset, batch_size=1, sampler=test_sampler)
+print("Number of training/test patches:", (len(train_indices),len(test_indices)))
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+net = ProbabilisticUnet(input_channels=1, num_classes=1, num_filters=[32,64,128,192], latent_dim=2, no_convs_fcomb=4, beta=10.0)
+net.to(device)
+optimizer = torch.optim.Adam(net.parameters(), lr=1e-4, weight_decay=0)
+epochs = 10
+for epoch in range(epochs):
+    for step, (patch, mask, _) in enumerate(train_loader):
+        patch = patch.to(device)
+        mask = mask.to(device)
+        mask = torch.unsqueeze(mask,1)
+        net.forward(patch, mask, training=True)
+        elbo = net.elbo(mask)
+        reg_loss = l2_regularisation(net.posterior) + l2_regularisation(net.prior) + l2_regularisation(net.fcomb.layers)
+        loss = -elbo + 1e-5 * reg_loss
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
---
+++ readme.md
@@ -0,0 +1,9 @@
+## A Probabilistic U-Net for Segmentation of Ambiguous Images
+
+Hantian Liu, Jiacheng Gao, Zhicheng Dong
+
+This is an implementation of [probabilistic U-Net](https://arxiv.org/pdf/1806.05034.pdf).
+
+We implemented model structure and some preprocessing of cityscape currently. 
+Now we can train the model with images from LIDC dataset with ```main.py```
+with default parameters.
